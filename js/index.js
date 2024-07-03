b3704baa660239eb323d9f30a9d25b119a9796a5
mohamadreza <mohamadreza1388.org@gmail.com>
2024-07-03 17:35:47 +0330

    add all section and end

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 $(function () {
-
+    $('.owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:3,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:7
+            }
+        }
+    })
+    $(".tab").hide()
+    $(".cat_btn:nth-child(1)").addClass('active')
+    show_tab()
+    $(".cat_btn").click(function () {
+        $(this).addClass("active").siblings(".cat_btn").removeClass("active")
+        show_tab()
+    })
+    function show_tab() {
+        let data = $(".cat_btn.active").attr("tab_name")
+        $(`div[tab_name=${data}`).fadeIn(500).siblings(".tab").fadeOut(0)
+    }
 })
